2924c11f1489130d6bffa2724cde4a6589696bff
dm16g12 <dan_friend@hotmail.co.uk>
2022-08-18 11:56:15 +0100

    Initial submission of login-page-frontend

---
+++ index.js
@@ -0,0 +1,10 @@
+const togglePassword = document.querySelector("#password-show-toggle");
+
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+});
